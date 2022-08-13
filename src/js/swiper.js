7ad539718f524b607fe2e95739bf7023d3392e89
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2022-08-13 11:25:31 +0300

    slider

---
+++ src/js/swiper.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper('.mySwiper', {
+  cssMode: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  mousewheel: true,
+  keyboard: true,
+});
