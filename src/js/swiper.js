a5a8f7d16d56d8952ef4b1a6a6e5de4aab811f40
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2022-08-14 13:37:20 +0300

    mobile slider

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,4 +1,9 @@
-var swiper = new Swiper('.mySwiper', {
+const arrow = document.getElementsByClassName('swiper-button-prev')[0];
+console.log('arrow :>> ', arrow);
+arrow.style.display = 'none';
+
+const swiper = new Swiper('.mySwiper', {
+  initialSlide: 1,
   cssMode: true,
   navigation: {
     nextEl: '.swiper-button-next',
@@ -7,9 +12,30 @@ var swiper = new Swiper('.mySwiper', {
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
-    dynamicBullets: true,
   },
+  on: {
+    slideChange: function () {
+      const arrow = document.getElementsByClassName('swiper-button-prev')[0];
+      arrow.style.display = 'none';
 
+      const activeIndex = swiper.activeIndex;
+      if (activeIndex == 0) {
+        arrow.style.fontSize = '5px';
+      } else {
+        arrow.style.fontSize = '20px';
+      }
+    },
+  },
   mousewheel: true,
   keyboard: true,
+});
+
+swiper.on('slideChange', function () {
+  console.log('object :>> ', swiper.activeIndex);
+  const activeIndex = swiper.activeIndex;
+  if (activeIndex == 0) {
+    arrow.style.display = 'none';
+  } else {
+    arrow.style.display = 'block';
+  }
 });
