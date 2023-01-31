14d27a0ff1ca1e965d0040d9e8977b45f66998a5
valera <valeriy.holovachko@pettersonapps.com>
2023-01-31 17:08:26 +0200

    [US-12,13]-Update the scrollbar per design,Align the text with images

--- src/js/Slider/arrows.js
+++ src/js/Slider/arrows.js
@@ -29,3 +29,31 @@ mainPageSlider.on('slideChange', function () {
     arrowNext.style.color = '#ffffff';
   }
 });
+
+reviewsSlider.on('slideChange', function () {
+  const realIndex = mainPageSlider.realIndex;
+
+  if (realIndex == 0) {
+    arrow.style.display = 'none';
+  } else {
+    arrow.style.display = 'block';
+    arrow.style.marginTop = '-47px';
+    arrow.style.marginLeft = '-10px';
+    arrow.style.fontSize = '20px';
+    arrow.style.color = '#ffffff';
+  }
+});
+
+reviewsSlider.on('slideChange', function () {
+  const realIndex = mainPageSlider.realIndex + 1;
+  const lastSlide = mainPageSlider.slides.length;
+
+  if (realIndex === lastSlide) {
+    arrowNext.style.display = 'none';
+  } else {
+    arrowNext.style.display = 'block';
+    arrowNext.style.marginTop = '-48px';
+    arrowNext.style.fontSize = '20px';
+    arrowNext.style.color = '#ffffff';
+  }
+});
